b33c8c405103f46e3f374a7e4eed81cbd3b0bb42
Carina Meireles <carinacmeireles@gmail.com>
2024-03-31 22:32:19 +0100

    add changeLocation function to change current map view and marker

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const timezoneResult = document.querySelector(".timezoneResult");
   const ispResult = document.querySelector(".ispResult");
   const apiCredits = `https://geo.ipify.org/service/account-balance?apiKey=${apiKey}`;
+  var lat;
+  var lng;
+
+  const map = L.map("map").locate({
+    setView: true,
+    maxZoom: 16,
+  });
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  map.on("locationfound", (e) => {
+    L.marker(e.latlng).addTo(map);
+  });
+
+  map.on("locationerror", (e) => {
+    console.error("Location denied or unavailable", e.message);
+  });
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -21,13 +42,20 @@ document.addEventListener("DOMContentLoaded", function () {
         method: "GET",
       });
       const data = await response.json();
-      console.log(data);
       ipResult.innerHTML = data.ip;
       locationResult.innerHTML = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
       timezoneResult.innerHTML = data.location.timezone;
       ispResult.innerHTML = data.isp;
+      lat = data.location.lat;
+      lng = data.location.lng;
+      changeLocation(lat, lng);
     } catch (error) {
       console.error(error);
     }
+  }
+
+  function changeLocation(lat, lng) {
+    map.setView([lat, lng]);
+    L.marker([lat, lng]).addTo(map);
   }
 });
